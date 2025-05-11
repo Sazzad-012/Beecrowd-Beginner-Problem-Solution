c94b102db36b9596bd2975a24631b801c068ca1c
Sazzad Rashid Chowdhury <175148215+Sazzad-012@users.noreply.github.com>
2025-05-11 23:39:24 +0600

    BEE 1004

---
+++ 1004-Simple Product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int A,B;
+    scanf("%d %d", &A, &B);
+
+    int product = (A*B);
+
+    printf("PROD = %d\n", product);
+    return 0;
+}
+
