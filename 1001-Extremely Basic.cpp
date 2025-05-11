a78b45b142d005dfe4dbab8b5acf80146c0b0b5c
Sazzad Rashid Chowdhury <175148215+Sazzad-012@users.noreply.github.com>
2025-05-11 23:07:06 +0600

    BEE 1001

---
+++ 1001-Extremely Basic.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int A, B;
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    int sum = A+B;
+
+    printf("X = %d\n", sum);
+    return 0;
+}
+
+
+
