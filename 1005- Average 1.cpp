9362ec7036bd9e86ce2942fdfc995b3df4149b5d
Sazzad Rashid Chowdhury <175148215+Sazzad-012@users.noreply.github.com>
2025-05-15 10:50:01 +0600

    BEE 1005

---
+++ 1005- Average 1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double A , B;
+    scanf("%lf %lf", &A, &B);
+
+    double Avg = (A * 3.5 + B * 7.5)/(3.5 + 7.5);
+
+    printf("MEDIA = %.5lf\n", Avg);
+    return 0;
+}
+
+
+
