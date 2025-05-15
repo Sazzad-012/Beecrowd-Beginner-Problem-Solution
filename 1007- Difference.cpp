01715f6ec66c5903f40684a95e259d92a8718e8d
Sazzad Rashid Chowdhury <175148215+Sazzad-012@users.noreply.github.com>
2025-05-15 11:43:54 +0600

    BEE 1007

---
+++ 1007- Difference.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int A, B, C, D;
+    scanf("%d %d %d %d", &A, &B, &C, &D);
+
+    int Difference = ((A * B) - (C * D));
+
+    printf("DIFERENCA = %d\n", Difference);
+    return 0;
+}
+
+
