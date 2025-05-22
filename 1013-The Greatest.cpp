a30f4a18866a79e431cc955c5192cb3db597e09a
Sazzad Rashid Chowdhury <175148215+Sazzad-012@users.noreply.github.com>
2025-05-22 10:44:03 +0600

    1013- The Greatest.cpp

--- 1013-The Greatest.cpp
+++ 1013-The Greatest.cpp
@@ -1,14 +1,16 @@
 #include<bits/stdc++.h>
+#include <stdlib.h>  // Required for abs()
 using namespace std;
-
 int main(){
     int a, b, s;
     scanf("%d %d %d", &a, &b, &s);
 
-    int MaiorAB = (a+b+abs(a-b))/2; //We first find the maximum of a and b using this trick and store it in MaiorAB.
-    int maior = (MaiorAB + s + abs(MaiorAB - s)) / 2; //Then we compare MaiorAB with s in the same way to get the maximum of all three values.
+    int MaiorAB = (a+b+abs(a-b))/2; // We first find the maximum of a and b using this trick and store it in MaiorAB.
+    int maior = (MaiorAB + s + abs(MaiorAB - s)) / 2; // Then we compare MaiorAB with s in the same way to get the maximum of all three values.
 
     printf("%d eh o maior\n", maior);
     return 0;
 }
+
+
 
